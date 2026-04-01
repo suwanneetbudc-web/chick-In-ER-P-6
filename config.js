922c718bb49e5f41905572eab493eedaccd00094
suwanneetbudc-web <suwanneet.budc@cpird.in.th>
2026-04-01 15:11:28 +0700

    Update config.js
    แก้ไข LIFF

--- config.js
+++ config.js
@@ -13,12 +13,11 @@
 const CONFIG = {
     // 🌐 URL ของ Google Apps Script (ตัวใหม่ที่รวมโค้ดแล้ว)
     // สามารถใช้ลิงก์เดียวกันได้เลย เพราะระบบ Backend แยกส่วนให้แล้ว
-    WEB_APP_API: "https://script.google.com/macros/s/AKfycbzUwMzks27DNUDGYQRwdnGeUWb7fvjBW1knwBp3sDQtdq1i3jQ60zmWkGFsZPh-ZDk_SA/exec",
+    WEB_APP_API: "https://script.google.com/macros/s/AKfycbxyQSQYcm5aUIoKccCns-xYW4cdeOnI14GWDGs1RzHUIdRPFvc9KWGr1fpIswrOmuP8/exec",
 
     // 📱 LIFF IDs
-    LIFF_ID_CHECKIN: "2008088008-48JzVeeQ",
-    LIFF_ID_HISTORY: "2008088008-jWGqwvvn",
-    LIFF_ID_REGISTER: "2008088008-RGaXeww4",
+    LIFF_ID_CHECKIN: "2009669874-D7NBKdeA",
+    LIFF_ID_HISTORY: "2009669874-c3E25raE",
 
     // 📍 การตั้งค่าพิกัดสถานที่ (ละติจูด, ลองจิจูด)
     TARGET_LATITUDE: 16.807157,
